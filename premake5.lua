fca41cdbc576c39270eea695258fe55bf70f318e
yangyuan333 <347065418@qq.com>
2023-03-08 18:21:32 +0800

    Update Windows

--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,13 @@ project "ImGui"
     targetdir ("bin/" .. dirname .. "/%{prj.name}")
     objdir ("bin-int/" .. dirname .. "/%{prj.name}")
 
+    includedirs
+    {
+        "./",
+        "../GLFW/include",
+        "../Glad/include"
+    }
+
     files
     {
         "imconfig.h",
@@ -16,7 +23,14 @@ project "ImGui"
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp"
+        "imgui_demo.cpp",
+        "examples/imgui_impl_glfw.cpp",
+        "examples/imgui_impl_glfw.h",
+        "examples/imgui_impl_opengl3.cpp",
+        "examples/imgui_impl_opengl3.h"
+    }
+    defines{
+        "IMGUI_IMPL_OPENGL_LOADER_GLAD"
     }
     
     filter "system:windows"
